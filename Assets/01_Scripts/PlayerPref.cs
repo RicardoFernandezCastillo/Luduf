31148bb7ca73bbb9c0643416a4d1593bf120c990
Bixter356 <rf924230@gmail.com>
2024-09-01 15:53:57 -0400

    Adicion de audio

--- Assets/01_Scripts/PlayerPref.cs
+++ Assets/01_Scripts/PlayerPref.cs
@@ -70,6 +70,45 @@ public class PlayerPref : MonoBehaviour
             player.XpCheck();
         }
     }
+    public static void LoadStats()
+    {
+        Player player = FindObjectOfType<Player>();
+        float h_aux = player.health;
+        player.health = PlayerPrefs.GetFloat("health", player.health);
+        player.maxHealth = PlayerPrefs.GetFloat("maxHealth", player.maxHealth);
+        player.level = PlayerPrefs.GetInt("level", player.level);
+        player.exp = PlayerPrefs.GetFloat("exp", player.exp);
+        player.maxExp = PlayerPrefs.GetFloat("maxExp", player.maxExp);
+        player.cuantityDashes = PlayerPrefs.GetInt("cuantityDashes", player.cuantityDashes);
+        player.timeToRechargeDash = PlayerPrefs.GetFloat("timeToRechargeDash", player.timeToRechargeDash);
+        player.total_Ammo = PlayerPrefs.GetInt("totalAmmo", player.total_Ammo);
+
+        if (h_aux > PlayerPrefs.GetFloat("health", player.health))
+        {
+            player.HealthCheck(false);
+            Debug.Log("Cargando Disminuir vida");
+        }
+        else
+        {
+            player.HealthCheck(true);
+            Debug.Log("Cargando Aumentar vida");
+        }
+
+        player.XpCheck();
+    }
+
+    public static void SaveStats()
+    {
+        Player player = FindObjectOfType<Player>();
+        PlayerPrefs.SetFloat("health", player.health);
+        PlayerPrefs.SetFloat("maxHealth", player.maxHealth);
+        PlayerPrefs.SetInt("level", player.level);
+        PlayerPrefs.SetFloat("exp", player.exp);
+        PlayerPrefs.SetFloat("maxExp", player.maxExp);
+        PlayerPrefs.SetInt("cuantityDashes", player.cuantityDashes);
+        PlayerPrefs.SetFloat("timeToRechargeDash", player.timeToRechargeDash);
+        PlayerPrefs.SetInt("totalAmmo", player.total_Ammo);
+    }
     //private void OnLevelWasLoaded(int level)
     //{
 
